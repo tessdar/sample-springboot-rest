f5e7d796a69c880f356c98c2127a32ffc5cea2dd
Kim WonChul <kimwonchul@Kims-MacBook-Pro.local>
2018-06-21 21:28:05 +0900

    better make

--- src/main/java/com/example/mapper/SampleMapper.java
+++ src/main/java/com/example/mapper/SampleMapper.java
@@ -3,6 +3,9 @@ package com.example.mapper;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface SampleMapper {
 	public List<Timestamp> getTicks();
 }
