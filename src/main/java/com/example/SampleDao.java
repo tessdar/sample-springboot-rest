fef79d254961163eb587e80483751f619417396f
Kim WonChul <kimwonchul@Kims-MacBook-Pro.local>
2018-06-21 20:52:30 +0900

    better make

--- src/main/java/com/example/SampleDao.java
+++ src/main/java/com/example/SampleDao.java
@@ -4,8 +4,8 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +13,8 @@ public class SampleDao {
 
 	private static String namespace = "com.example.dao";
 	
-	@Autowired
-	@Qualifier("sqlSession")
+//	@Autowired
+//	@Qualifier("sqlSession")
 	private SqlSessionTemplate sqlSession;
 	
 	public List<Timestamp> getTicks() throws Exception {
