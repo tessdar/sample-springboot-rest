b1a1757f192ff3b2e8ae0ff42e294e7d87b14ecd
Kim WonChul <kimwonchul@Kims-MacBook-Pro.local>
2018-06-21 20:57:59 +0900

    better make

--- src/main/java/com/example/DBConfig.java
+++ src/main/java/com/example/DBConfig.java
@@ -1,38 +1,38 @@
 package com.example;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
 
 //import org.apache.ibatis.session.SqlSessionFactory;
 //import org.mybatis.spring.SqlSessionFactoryBean;
 //import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 //import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
-@EnableTransactionManagement
+//@Configuration
+//@EnableTransactionManagement
 public class DBConfig {
 
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-
-	@Bean(name = "dataSource")
-	public DataSource dataSource() throws SQLException {
-		if (dbUrl == null || dbUrl.isEmpty()) {
-			return new HikariDataSource();
-		} else {
-			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl(dbUrl);
-			return new HikariDataSource(config);
-		}
-	}
+//	@Value("${spring.datasource.url}")
+//	private String dbUrl;
+//
+//	@Bean(name = "dataSource")
+//	public DataSource dataSource() throws SQLException {
+//		if (dbUrl == null || dbUrl.isEmpty()) {
+//			return new HikariDataSource();
+//		} else {
+//			HikariConfig config = new HikariConfig();
+//			config.setJdbcUrl(dbUrl);
+//			return new HikariDataSource(config);
+//		}
+//	}
 
 //	@Bean(name = "sqlSessionFactory")
 //	public SqlSessionFactory sqlSessionFactory() throws Exception {
